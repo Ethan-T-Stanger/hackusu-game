22e2439f4a67bde3938d8798837bbe79d1fdea2f
Ethan T. Stanger <ethan@lightningkite.com>
2024-03-01 18:07:27 -0700

    movement changes

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,24 @@ use bevy::render::{
 use bevy::window::WindowResized;
 
 const RESOLUTION: Extent3d = Extent3d {
-    width: 256,
-    height: 256,
+    width: 512,
+    height: 288,
     depth_or_array_layers: 1,
 };
-// const PIXEL_PERFECT_LAYER: RenderLayers = RenderLayers::layer(0);
+
 const HIGH_RES_LAYER: RenderLayers = RenderLayers::layer(1);
 
+const BOOST_ACCELERATION_SPEED: f32 = 4.0;
+const PASSIVE_ACCELERATION_SPEED: f32 = 3.0;
+const MAX_SPEED: f32 = 145.0;
+const ROTATION_SPEED: f32 = 7.0;
+const DRAG: f32 = 0.998;
+
 #[derive(Component)]
-struct Velocity {
-    x: f32,
-    y: f32,
-}
+struct Player;
+
+#[derive(Component)]
+struct Velocity(Vec2);
 
 #[derive(Component)]
 struct InGameCamera;
@@ -36,7 +42,7 @@ fn main() {
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .insert_resource(Msaa::Off)
         .add_systems(Startup, (setup_player, setup_camera))
-        .add_systems(Update, fit_canvas)
+        .add_systems(Update, (fit_canvas, move_player))
         .run();
 }
 
@@ -46,8 +52,52 @@ fn setup_player(mut commands: Commands, asset_server: Res<AssetServer>) {
             texture: asset_server.load("graphics/player.png"),
             ..default()
         },
-        Velocity { x: 0.0, y: 0.0 },
+        Velocity(Vec2::ZERO),
+        Player,
     ));
+}
+
+fn move_player(
+    time: Res<Time>,
+    input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<(&Player, &mut Transform, &mut Velocity)>,
+) {
+    let (_player, mut transform, mut velocity) = query.single_mut();
+
+    if input.pressed(KeyCode::KeyA) {
+        transform.rotate_z(ROTATION_SPEED * time.delta_seconds());
+    }
+    if input.pressed(KeyCode::KeyD) {
+        transform.rotate_z(-ROTATION_SPEED * time.delta_seconds());
+    }
+
+    let axis_angle = transform.rotation.to_axis_angle();
+    let current_rotation = axis_angle.0.z * axis_angle.1;
+
+    if input.pressed(KeyCode::KeyK) {
+        let acceleration_vector = rotation_to_vector(current_rotation) * BOOST_ACCELERATION_SPEED;
+        velocity.0 += acceleration_vector;
+    }
+
+    let velocity_speed = velocity.0.length();
+    velocity.0 += rotation_to_vector(current_rotation) * PASSIVE_ACCELERATION_SPEED;
+    velocity.0 = velocity.0.normalize()
+        * if velocity_speed > MAX_SPEED {
+            MAX_SPEED
+        } else {
+            velocity_speed
+        };
+    velocity.0 *= DRAG;
+
+    transform.translation.x += velocity.0.x * time.delta_seconds();
+    transform.translation.y += velocity.0.y * time.delta_seconds();
+}
+
+fn rotation_to_vector(rotation: f32) -> Vec2 {
+    Vec2 {
+        x: rotation.cos(),
+        y: rotation.sin(),
+    }
 }
 
 fn setup_camera(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
